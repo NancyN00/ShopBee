28eafcbea538742de117b3ad999781869cd189ae
NancyN00 <nancynyabuti10@gmail.com>
2024-02-14 08:13:00 +0300

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.shopbee"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.shopbee"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -25,6 +25,10 @@ android {
                 "proguard-rules.pro"
             )
         }
+    }
+
+    buildFeatures{
+        viewBinding = true
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -41,6 +45,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/shopbee/auth/StartActivity.kt
@@ -0,0 +1,27 @@
+package com.example.shopbee.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.shopbee.R
+import com.example.shopbee.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityStartBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.clic.setOnClickListener {
+            val intent = Intent(this@StartActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this, "Clicker", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
