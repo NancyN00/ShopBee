e558b6609cd144f9899f7886b4c9f6675d9f1bee
NancyN00 <nancynyabuti10@gmail.com>
2024-02-21 16:52:15 +0300

    added

--- app/src/main/java/com/example/shopbee/auth/StartActivity.kt
+++ app/src/main/java/com/example/shopbee/auth/StartActivity.kt
@@ -12,12 +12,18 @@ class StartActivity : AppCompatActivity() {
     private lateinit var binding : ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.clic.setOnClickListener {
+        binding.loginBtn.setOnClickListener {
             val intent = Intent(this@StartActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.regBtn.setOnClickListener {
+            val intent = Intent(this@StartActivity, RegistrationActivity::class.java)
             startActivity(intent)
             finish()
             Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
