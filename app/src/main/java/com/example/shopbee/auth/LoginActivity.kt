4e4833ebe66f1368a6cbafc00fba0348aec404f2
NancyN00 <nancynyabuti10@gmail.com>
2024-02-25 17:56:38 +0300

    added email and password required

--- app/src/main/java/com/example/shopbee/auth/LoginActivity.kt
+++ app/src/main/java/com/example/shopbee/auth/LoginActivity.kt
@@ -3,26 +3,55 @@ package com.example.shopbee.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.widget.Toast
 import com.example.shopbee.R
 import com.example.shopbee.databinding.ActivityLoginBinding
 import com.example.shopbee.fragments.HomeFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.loginBtn.setOnClickListener {
-            val intent = Intent(this@LoginActivity, HomeFragment::class.java)
-            startActivity(intent)
-        }
+        auth = Firebase.auth
 
         binding.donthaveAcc.setOnClickListener {
             val intent = Intent(this@LoginActivity, RegistrationActivity::class.java)
             startActivity(intent)
+        }
+
+        fun String.toEditable(): Editable = Editable.Factory.getInstance().newEditable(this)
+
+         binding.loginmailTxt.text = "ancyy@gmail.com".toEditable()
+         binding.loginpassTxt.text = "12345".toEditable()
+
+        binding.loginBtn.setOnClickListener {
+
+            val logmail = binding.loginmailTxt.text.toString().trim()
+            val logpass = binding.loginpassTxt.text.toString().trim()
+
+
+            when {
+
+                TextUtils.isEmpty(logmail) -> binding.loginmailTxt.error = "Email Required"
+                TextUtils.isEmpty(logpass) -> binding.loginpassTxt.error = "Password Required"
+
+                else -> {
+                    val intent = Intent(this@LoginActivity, StartActivity::class.java)
+                    startActivity(intent)
+                   Toast.makeText(this, "success", Toast.LENGTH_SHORT).show()
+                }
+            }
+
         }
 
     }
--- app/src/main/java/com/example/shopbee/auth/RegistrationActivity.kt
+++ app/src/main/java/com/example/shopbee/auth/RegistrationActivity.kt
@@ -3,26 +3,56 @@ package com.example.shopbee.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.example.shopbee.R
 import com.example.shopbee.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegistrationActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityRegistrationBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.regBtn.setOnClickListener {
-            val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
-            startActivity(intent)
-        }
+        auth = Firebase.auth
 
         binding.haveAcc.setOnClickListener {
             val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
             startActivity(intent)
 
+        }
+
+        binding.regBtn.setOnClickListener {
+            val usermail = binding.regmailTxt.text.toString().trim()
+            val userpass = binding.regpassTxt.text.toString().trim()
+
+               if (usermail.isEmpty()){
+                   binding.regmailTxt.error = "Email Required"
+                   return@setOnClickListener
+               }
+
+            if(userpass.isEmpty()){
+                binding.regpassTxt.error = "Password Required"
+                return@setOnClickListener
+            }
+            auth.createUserWithEmailAndPassword(usermail, userpass).addOnCompleteListener {
+                 if(it.isSuccessful){
+                     val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
+                     startActivity(intent)
+                     Toast.makeText(this, "Registered Successfully", Toast.LENGTH_SHORT).show()
+
+                 }else{
+
+                     Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                 }
+
+            }
         }
 
     }
